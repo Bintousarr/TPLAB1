e55cb5669ecb1b7c2f4f74e506189ef3e7060e32
Bintousarr <camarabintousarr@gmail.com>
2024-04-26 19:07:06 +0000

    Premier commit

--- LAB1.py
+++ LAB1.py
@@ -1,27 +1,18 @@
 import math
 
+#fonction distance
+def distance(point1, point2):
+    return math.sqrt(sum((x2 - x1) ** 2 for x1, x2 in zip(point1, point2)))
 
-def calc_distance(point1, point2):
-    # Extraits les coordonnees des points
-    x1, y1, z1 = point1
-    x2, y2, z2 = point2
-
-    ## Calcul de la distance
-    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
-
-    return distance
+# fonction my_map
+def my_map(func, list1, list2):
+    return [func(*args) for args in zip(list1, list2)]
 
 
 point1 = [(1.0, 1.0, 1.0), (2.0, 2.0, 2.0), (3.0, 3.0, 3.0)]
 point2 = [(4.0, 4.0, 4.0), (5.0, 5.0, 5.0), (6.0, 6.0, 6.0)]
 
 
-### Definition de ma fonction
-def my_map(func, list1, list2):
-    return [func(element1, element2) for element1, element2 in zip(list1, list2)]
-
-
-#### Calcul des distances
-distances = my_map(calc_distance, point1, point2)
+distances = my_map(distance, point1, point2)
 
 print(distances)
